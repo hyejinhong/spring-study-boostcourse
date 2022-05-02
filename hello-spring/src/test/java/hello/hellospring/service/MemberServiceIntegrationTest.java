ef9b37c8d6e755a81edc8a3f4e885edcada501a4
hyejinhong <hhj732@naver.com>
2022-05-02 22:45:23 +0900

    스프링 데이터 JPA

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -23,7 +23,7 @@ class MemberServiceIntegrationTest {
     void 회원가입() throws Exception {
         // given
         Member member = new Member();
-        member.setName("떡볶이!");
+        member.setName("떡볶1");
 
         // when
         Long saveId = memberService.join(member);
